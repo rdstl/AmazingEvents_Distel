51d808d320574e16966df5a8447c90875899ca5f
Rolando Distel <rdstl7529@gmail.com>
2023-08-27 22:54:17 -0300

    se agregaron archivos .js para agregar las tarjetas de manera dinamica

---
+++ js/upcoming_events.js
@@ -0,0 +1,28 @@
+
+function makeCards(events, idContenedor)
+{
+
+   let fecharef = data.currentDate
+   for(let event of data.events)
+      if (fecharef<event.date){ 
+      {
+         
+         let card = `
+         <div class="col-12 col-sm-6 col-md-4 col-xl-3">
+            <div class="card">
+               <img src="${event.image}" class="card-img-top" alt="${event.name}">
+               <div class="card-body">
+                  <h5 class="card-title">${event.name}</h5>
+                  <p class="card-text">${event.description}</p>
+                  <p class="card-text">Date: ${event.date}</p>
+                  <p class="card-text">Price: $ ${event.price}.-</p>
+                  <a href="/details.html" class="mt-auto btn btn-danger text-white detalle">Details</a>
+                </div>
+            </div>
+         </div>`;
+         document.getElementById(idContenedor).innerHTML += card;
+      }
+   }
+};
+
+makeCards(data, 'upcomingEventsContainer');
